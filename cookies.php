b29596861e679055b1392dd1c2e6b63b48b86cf8
nasua <acollazocacho@gmail.com>
2024-01-17 12:56:06 +0100

    completo

---
+++ cookies.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cookies</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+</head>
+<body>
+    <div class="container-fluid">
+        <h1 class="text-center">Introduzca datos para crear una cookie</h1>
+
+        <div class="row justify-content-center">
+            <div class="col-sm-12 col-md-6">
+                <form method="post">
+                    <div class="form-group mb-4 ">
+                        <label class="form-label" for="cookieName">Cookie name</label>
+                        <input type="text" id="cookieName" class="form-control" name="cookieName"  />
+
+                    </div>
+                    <div class="form-group mb-4">
+                        <label class="form-label" for="cookieValue">Cookie value</label>
+                        <input type="text" id="cookieValue" class="form-control" name="cookieValue"  />
+
+                    </div>
+                    <div class="form-group mb-4">
+                        <label class="form-label" for="cookieTime">Cookie expiration seconds</label>
+                        <input type="number" id="cookieTime" class="form-control" name="cookieTime" value ="0" min="0"  />
+
+                    </div>
+                    
+                    <input type="submit" class="btn btn-primary btn-block mb-4" value="Añadir cookie" name ="crearCookie"></button>
+                    <?php
+                    // Mostrar el botón de borrar solo si existen cookies
+                    if (isset($_POST["cookieName"]) && isset($_COOKIE[$_POST["cookieName"]])) {
+                        echo '<input type="submit" class="btn btn-danger btn-block mb-4" value="Borrar cookie" name="borrarCookie">';
+                    }
+                    ?>
+                </form>
+            </div>
+        </div>
+        <?php
+        require_once "tabla_cookies.php";
+
+        // Cogemos los valores del formulario y los guardamos en variables
+        if (isset($_POST["cookieName"]) && ($_POST["cookieValue"])) {
+            $cookieValue = $_POST["cookieValue"];
+            $cookieName = $_POST["cookieName"];
+            $cookieTime = $_POST["cookieTime"];
+            if ($cookieTime == null) {
+                $cookieTime = 0;}
+        }
+        // funciones para crear y borrar cookies
+        function crearCookie($cookieValue,$cookieName,$cookieTime){
+            setcookie($cookieName, $cookieValue, time() +$cookieTime);
+        }
+        function borrarCookie($cookieName)
+        {
+            setcookie($cookieName, false, time() -3600);
+        }
+        //Operaciones Crear o Borrar cookie
+        if (isset($_POST["crearCookie"])) {
+                crearCookie($cookieValue,$cookieName,$cookieTime);
+            }
+        if (isset($_POST["borrarCookie"]) && ($_POST["cookieName"])) {
+                borrarCookie( $cookieName );
+                unset($_COOKIE[$cookieName]);
+            }
+        //Mostramos la tabla de cookies
+        mostrarTablaCookies();
+        ?>
+        
+</body>
+</html>
---
+++ tabla_cookies.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>tabla cookies</title>
+</head>
+<body>
+
+<?php 
+
+//var_dump($_POST);
+//var_dump($_COOKIE);
+
+    function mostrarTablaCookies() {
+        if (!empty($_COOKIE) && !isset($_POST['borrar_cookies'])) {
+            echo "<h3>Cookies creadas:</h3>";
+            echo "<table class='table table-striped'>";
+            echo "<tr><th>Nombre</th><th>Valor</th></tr>";
+            foreach ($_COOKIE as $nombre => $valor) {
+                echo "<tr><td>$nombre</td><td>$valor</td></tr>";
+            }
+            echo "</table>";
+            echo "<form method='post' action='cookies-prueba.php'>";
+            echo "<input type='submit' name='borrar_cookies' value='Borrar Cookies'>";
+            echo "</form>";
+        } else {
+            echo "<p>No hay cookies creadas.</p>";
+        }
+    }
+
+
+    if (isset($_POST['borrar_cookies'])) {
+        foreach ($_COOKIE as $key => $value) {
+            setcookie($key, '', time() - 1);
+        }
+
+    }
+
+?>
+    
+</body>
+</html>
